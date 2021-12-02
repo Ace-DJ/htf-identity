d742d05ac7255b16b6aa2d3118166522f4c8b5b9
Daniel Deboeverie <daniel.deboeverie@student.kdg.be>
2021-12-02 11:08:17 +0100

    decode string done

--- src/main/java/be/springtheory/htf/investigation/DecodeInvestigation.java
+++ src/main/java/be/springtheory/htf/investigation/DecodeInvestigation.java
@@ -1,4 +1,36 @@
 package be.springtheory.htf.investigation;
 
-public class DecodeInvestigation {
+import org.springframework.stereotype.Component;
+
+@Component
+public class DecodeInvestigation implements Investigation {
+    String[] language = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+            "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x",
+            "y", "z"};
+
+    String[] morse = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",
+            ".---", "-.-", ".-..", "--", "-.", "---", ".---.", "--.-", ".-.",
+            "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+
+
+    @Override
+    public String solve(String investigationParameters) {
+        String[] paramMorse = investigationParameters.split("\\s+");
+        StringBuilder builder = new StringBuilder("");
+        for (String paramMorseLetter : paramMorse) {
+            for (int i = 0; i < morse.length; i++) {
+                if (morse[i].equals(paramMorseLetter)) {
+                    builder.append(language[i]).append(" ");
+                    i = morse.length;
+                }
+            }
+        }
+        builder.deleteCharAt(builder.length() -1);
+        return builder.toString();
+    }
+
+    @Override
+    public String getInvestigationName() {
+        return null;
+    }
 }
--- src/test/java/be/springtheory/htf/HtfApplicationTests.java
+++ src/test/java/be/springtheory/htf/HtfApplicationTests.java
@@ -8,6 +8,7 @@ class HtfApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
