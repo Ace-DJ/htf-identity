67418b7a451cd3694e7faff4ebb70947c4b1d91a
acedejong <ace.dejong@student.kdg.be>
2021-12-02 09:48:43 +0100

    Initial commit

---
+++ src/main/java/be/springtheory/htf/HtfApplication.java
@@ -0,0 +1,13 @@
+package be.springtheory.htf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/springtheory/htf/HtfApplicationTests.java
@@ -0,0 +1,13 @@
+package be.springtheory.htf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
